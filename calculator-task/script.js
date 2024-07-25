ef66ec898b7fa356af1a6eb095f301dcb5648084
salar patel <70138079+salarpatel7@users.noreply.github.com>
2024-07-25 06:23:24 +0000

    "1\n2,3" should return 6

--- calculator-task/script.js
+++ calculator-task/script.js
@@ -1,9 +1,11 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    const numArray = numbers.split(",");
+    const numArray = numbers.split(/[\n,]/);
     return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
 console.log(add("")); // Output: 0
 console.log(add("1")); // Output: 1
 console.log(add("1,5")); // Output: 6
+console.log(add("1,2,3,4,5")); // Output: 15
+console.log(add("1\n2,3")); // Output: 6
