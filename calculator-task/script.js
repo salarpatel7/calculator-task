d3f2184badb285abf347a129dd937dd5a574a100
salar patel <70138079+salarpatel7@users.noreply.github.com>
2024-07-25 06:18:18 +0000

    Input: “1,5”, Output: 6

--- calculator-task/script.js
+++ calculator-task/script.js
@@ -1,7 +1,9 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return parseInt(numbers);
+    const numArray = numbers.split(",");
+    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
 console.log(add("")); // Output: 0
 console.log(add("1")); // Output: 1
+console.log(add("1,5")); // Output: 6
